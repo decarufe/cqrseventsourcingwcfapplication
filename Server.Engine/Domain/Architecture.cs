7fdcc8dad47af0847eab9921ffacf6a2e566597f
Eric De Carufel <decarufe@users.noreply.github.com>
2013-06-28 13:09:29 -0400

    Event stuff

---
+++ Server.Engine/Domain/Architecture.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Server.Engine.Events;
+using SimpleCqrs.Domain;
+
+namespace Server.Engine.Domain
+{
+  class Architecture : AggregateRoot
+  {
+    private string _name;
+
+    public Architecture()
+    {
+    }
+
+    public Architecture(string name)
+    {
+      CreateArchitecture();
+      ChangeName(name);
+    }
+
+    private void CreateArchitecture()
+    {
+      Id = new Guid("bozo");
+      Apply(new ArchitectureCreatedEvent());
+    }
+
+    public void OnArchitectureCreated(ArchitectureCreatedEvent architectureCreatedEvent)
+    {
+      Id = architectureCreatedEvent.AggregateRootId;
+    }
+
+    public void OnNameChanged(NameChangedEvent nameChangedEvent)
+    {
+      _name = nameChangedEvent.NewName;
+    }
+
+    public void ChangeName(string name)
+    {
+      Apply(new NameChangedEvent
+      {
+        NewName = name
+      });
+    }
+  }
+}
---
+++ Server.Engine/Events/NameChangedEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimpleCqrs.Eventing;
+
+namespace Server.Engine.Events
+{
+  class NameChangedEvent : DomainEvent
+  {
+    public string NewName { set; get; }
+  }
+}
