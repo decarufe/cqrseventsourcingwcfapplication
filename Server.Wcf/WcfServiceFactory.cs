916f14722eb80d0438e23ff7167379ddec50b10c
Eric De Carufel <decarufe@users.noreply.github.com>
2013-06-28 12:40:35 -0400

    Fix service creation

--- Server.Wcf/WcfServiceFactory.cs
+++ Server.Wcf/WcfServiceFactory.cs
@@ -3,13 +3,24 @@ using System.ServiceModel;
 using Microsoft.Practices.Unity;
 using Server.Contracts;
 using Server.Engine;
-using SimpleCqrs.Commanding;
 using Unity.Wcf;
 
 namespace Server.Wcf
 {
   public class WcfServiceFactory : UnityServiceHostFactory
   {
+    private TrainingRuntime _runtime;
+
+    protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+    {
+      _runtime = new TrainingRuntime();
+      _runtime.Start();
+      
+      var unityContainer = _runtime.ServiceLocator.Container;
+      ConfigureContainer(unityContainer);
+      return new UnityServiceHost(unityContainer, serviceType, baseAddresses);
+    }
+
     protected override void ConfigureContainer(IUnityContainer container)
     {
       container.RegisterType<IService1, ServiceImpl>();
