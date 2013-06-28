b3d7efb168a424fa6c03c94b53580cf04f6acb2b
Eric De Carufel <Eric De Carufel@localhost>
2013-06-28 13:21:32 -0400

    Working event

--- Server.Engine/CommandHandlers/CommandHandler.cs
+++ Server.Engine/CommandHandlers/CommandHandler.cs
@@ -1,15 +1,25 @@
 using System;
 using Server.Engine.Commands;
+using Server.Engine.Domain;
 using SimpleCqrs.Commanding;
+using SimpleCqrs.Domain;
 
 namespace Server.Engine.CommandHandlers
 {
   public class CommandHandler :
     IHandleCommands<SetNameCommand>
   {
+    private readonly IDomainRepository _repository;
+
+    public CommandHandler(IDomainRepository repository)
+    {
+      _repository = repository;
+    }
+
     public void Handle(ICommandHandlingContext<SetNameCommand> handlingContext)
     {
-      Console.WriteLine((string) handlingContext.Command.Name);
+      var architecture = new Architecture(handlingContext.Command.Id, handlingContext.Command.Name);
+      _repository.Save(architecture);
     }
   }
 }
---
+++ Server.Engine/Domain/Architecture.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Server.Engine.Events;
+using SimpleCqrs.Domain;
+
+namespace Server.Engine.Domain
+{
+  public class Architecture : AggregateRoot
+  {
+    public string _name;
+
+    public Architecture()
+    {
+    }
+
+    public Architecture(Guid id, string name)
+    {
+      CreateArchitecture(id);
+      ChangeName(name);
+    }
+
+    private void CreateArchitecture(Guid id)
+    {
+      Apply(new ArchitectureCreatedEvent(id));
+    }
+
+    public void OnArchitectureCreated(ArchitectureCreatedEvent architectureCreatedEvent)
+    {
+      Id = architectureCreatedEvent.AggregateRootId;
+    }
+
+    public void OnNameChanged(NameChangedEvent nameChangedEvent)
+    {
+      _name = nameChangedEvent.NewName;
+    }
+
+    public void ChangeName(string newName)
+    {
+      Apply(new NameChangedEvent()
+      {
+        NewName = newName
+      });
+    }
+  }
+}
