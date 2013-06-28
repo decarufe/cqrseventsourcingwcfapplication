b66304d482763d810162d45b865eb44e9388c9c8
Eric De Carufel <decarufe@users.noreply.github.com>
2013-06-28 14:00:33 -0400

    Implemement persistance

---
+++ Server.ReadModels/Persistance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Server.ReadModels
+{
+  public class Persistance : IPersistance
+  {
+    private Dictionary<Guid, ArchitectureView> _architectureTable = new Dictionary<Guid, ArchitectureView>();
+
+    private static IPersistance _persistance;
+
+    public static IPersistance Instance
+    {
+      get { 
+        if (_persistance == null)
+          _persistance = new Persistance();
+
+        return _persistance;
+      }
+    }
+
+    public void Save(Guid id, ArchitectureView architectureView)
+    {
+      if (_architectureTable.ContainsKey(id))
+        _architectureTable[id] = architectureView;
+      else
+        _architectureTable.Add(id, architectureView);
+    }
+
+    public ArchitectureView Get(Guid id)
+    {
+      return _architectureTable[id];
+    }
+  }
+}
