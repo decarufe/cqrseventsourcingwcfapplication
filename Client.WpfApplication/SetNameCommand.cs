a35939fb0878081d021000e8760c56d98d7a6433
Eric De Carufel <decarufe@users.noreply.github.com>
2013-07-12 14:46:14 -0400

    Merged with Eric's work

---
+++ Client.WpfApplication/SetNameCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Input;
+using Client.WpfApplication.CqrsServiceReference;
+
+namespace Client.WpfApplication
+{
+  public class SetNameCommand : ICommand
+  {
+    private readonly IRefresh _refresher;
+    private bool _executing;
+
+    public SetNameCommand(IRefresh refresher)
+    {
+      _refresher = refresher;
+    }
+
+    public void Execute(object parameter)
+    {
+      try
+      {
+        _executing = true;
+        OnCanExecuteChanged();
+
+        var readModelEntity = (DomainModelDto) parameter;
+
+        using (var client = new CqrsServiceClient())
+        {
+          client.SetName(Guid.Parse(readModelEntity.ReadModelId), readModelEntity.Name);
+        }
+        _refresher.Refresh();
+      }
+      finally
+      {
+        _executing = false;
+        OnCanExecuteChanged();
+      }
+    }
+
+    public bool CanExecute(object parameter)
+    {
+      return !_executing;
+    }
+
+    public event EventHandler CanExecuteChanged;
+
+    protected virtual void OnCanExecuteChanged()
+    {
+      var handler = CanExecuteChanged;
+      if (handler != null) handler(this, EventArgs.Empty);
+    }
+  }
+}
---
+++ Server.Engine/ReadModelRepository.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+using MongoRepository;
+using Server.Contracts;
+
+namespace Server.Engine
+{
+  [UsedImplicitly]
+  public class ReadModelRepository : MongoRepository<ReadModelEntity>
+  {
+    
+  }
+}
